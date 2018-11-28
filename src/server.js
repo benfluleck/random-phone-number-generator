5647e5756cbddf6c11c0655807a78bd0622cb080
Benny Ogidan <bennyogidan@Andelas-MacBook-Pro-8.local>
2018-11-28 14:32:12 +0100

    feat(implement-api-routes): Implement file download functionality
    - Generate phone numbers
    - Generate route for download functionality
    - Implement validators
    - Implement eslint functionality

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 
 import { indexRouter } from './routes/index';
 import setGlobalMiddleware from './middleware/setGlobalMiddleware';
